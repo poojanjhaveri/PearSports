b0a6043d2b2f4b14f0cd39da78998f1d656f43f9
Garima <garimaag@usc.edu>
2014-03-05 11:39:06 -0800

    Beginning personalize recommend workout

---
+++ PearSportsTrainerApp/PearSportsTrainerApp/GA_PersonalizeWorkoutViewController.h
@@ -0,0 +1,13 @@
+//
+//  GA_PersonalizeWorkoutViewController.h
+//  PearSportsTrainerApp
+//
+//  Created by Garima Aggarwal on 3/5/14.
+//  Copyright (c) 2014 Poojan Jhaveri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GA_PersonalizeWorkoutViewController : UIViewController
+
+@end
