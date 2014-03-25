1d063c90c9d602e8c85acd90a554ccba925bd9f0
devonmeyer <devonmeyer212@gmail.com>
2014-03-24 20:02:50 -0700

    Added comments explaining new workoutArray

--- PearSportsTrainerApp/PearSportsTrainerApp/PJ_Client.h
+++ PearSportsTrainerApp/PearSportsTrainerApp/PJ_Client.h
@@ -22,6 +22,29 @@
 @property  NSString * weight;
 @property  NSString * imageName;
 
+
+/*
+ 
+ WorkoutArray is an Array of Arrays. The first order of arrays corresponds to a particular week, the second order corresponds to a particular day.
+ 
+ The indexes for the first order are...
+ 
+ workoutArray[0] = An array of workouts (unscheduled, completed, missed, scheduled) corresponding to two weeks ago.
+ workoutArray[1] = An array of workouts corresponding to last week.
+ workoutArray[2] = An array of workouts corresponding to the current week.
+ 
+ The indexes for the second order are...
+ 
+ workoutArray[N][0] = Sunday of the N'th week.
+ workoutArray[N][1] = Monday of the N'th week.
+ ...
+ workoutArray[N][6] = Saturday of the N'th week.
+ 
+ 
+ Therefore, workoutArray[1][2] is the workout corresponding to Tuesday of last week.
+ 
+ */
+
 @property (nonatomic, strong) NSMutableArray * workoutArray;
 
 
