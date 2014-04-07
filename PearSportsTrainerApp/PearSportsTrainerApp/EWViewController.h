75a27ec018804469ffee0188efd0a9cfec9a5af3
Elsen Wiraatmadja <ewiraatm@usc.edu>
2014-04-07 11:31:11 -0700

    photo

--- PearSportsTrainerApp/PearSportsTrainerApp/EWViewController.h
+++ PearSportsTrainerApp/PearSportsTrainerApp/EWViewController.h
@@ -11,16 +11,22 @@
 #import <UIKit/UIKit.h>
 #import "UIBubbleTableViewDataSource.h"
 #import <AVFoundation/AVFoundation.h>
+#import <CoreGraphics/CoreGraphics.h>
+#import <MobileCoreServices/MobileCoreServices.h>
 
-@interface EWViewController : UIViewController <UIBubbleTableViewDataSource, AVAudioRecorderDelegate, AVAudioPlayerDelegate>
+@interface EWViewController : UIViewController <UIBubbleTableViewDataSource, AVAudioRecorderDelegate, AVAudioPlayerDelegate, UIActionSheetDelegate,UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 
 @property (strong, nonatomic) AVAudioRecorder *audioRecorder;
 @property (strong, nonatomic) AVAudioPlayer *audioPlayer;
 @property (strong, nonatomic) AVAudioSession *audioSession;
 @property (nonatomic, assign) BOOL recording;
 @property (strong, nonatomic) NSArray *arr;
-
+@property (strong, nonatomic) UIImage *selectedImage;
 @property (strong) UIActivityIndicatorView * activityIndicator;
+@property BOOL newMedia;
+
+@property(nonatomic,retain)IBOutlet UIImageView *imageView;
+
 
 - (IBAction)recordVoice:(id)sender;
 - (NSString*) dateString;
